6938e839abe57d02d20963808cab74764ab44cb2
Sarah <sarah.touati@lengow.com>
2016-02-12 14:20:57 +0100

    Marketplace carrier management

--- classes/controllers/LengowOrderSettingController.php
+++ classes/controllers/LengowOrderSettingController.php
@@ -1,5 +1,4 @@
 <?php
-ini_set('display_errors', 1);
 /**
  * Copyright 2016 Lengow SAS.
  *
--- views/js/lengow/order_setting.js
+++ views/js/lengow/order_setting.js
@@ -20,13 +20,15 @@
         });
 
         $('#add_country').on('click', '.delete_lengow_default_carrier', function () {
-            var href = $('.lengow_default_carrier').attr('data-href');
-            $.ajax({
-                url: href,
-                method: 'POST',
-                data: {action: 'delete_country', id_country: $(this).attr('data-id-country')},
-                dataType: 'script'
-            });
+            if (confirm('Are you sure ?')) {
+                var href = $('.lengow_default_carrier').attr('data-href');
+                $.ajax({
+                    url: href,
+                    method: 'POST',
+                    data: {action: 'delete_country', id_country: $(this).attr('data-id-country')},
+                    dataType: 'script'
+                });
+            }
 
             return false;
         });
@@ -40,6 +42,39 @@
 
             return false;
 
+        });
+
+        $('#add_marketplace_country').on('change', '.carrier', function () {
+            if ($(this).val() !== "") {
+                $(this).parents('.marketplace_carrier ').removeClass('no_carrier');
+            } else {
+                $(this).parents('.marketplace_carrier ').addClass('no_carrier');
+            }
+
+            return false;
+
+        });
+
+        $(".navigation ul.subMenu").hide();
+
+        $(".navigation li.toggleSubMenu span").each( function () {
+            $(this).replaceWith('<a href="" title="Afficher le sous-menu">Hello<\/a>') ;
+        } ) ;
+
+        // On modifie l'évènement "click" sur les liens dans les items de liste
+        // qui portent la classe "toggleSubMenu" :
+        $(".navigation li.toggleSubMenu > a").click( function () {
+            // Si le sous-menu était déjà ouvert, on le referme :
+            if ($(this).next("ul.subMenu:visible").length != 0) {
+                $(this).next("ul.subMenu").slideUp("normal");
+            }
+            // Si le sous-menu est caché, on ferme les autres et on l'affiche :
+            else {
+                $(".navigation ul.subMenu").slideUp("normal");
+                $(this).next("ul.subMenu").slideDown("normal");
+            }
+            // On empêche le navigateur de suivre le lien :
+            return false;
         });
     });
 
