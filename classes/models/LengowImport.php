7bf034098224a1f4c62f17169517ab188ee638ed
benjamin <benjamin.le-neve@lengow>
2016-03-29 17:56:34 +0200

    Change api call

--- classes/models/LengowImport.php
+++ classes/models/LengowImport.php
@@ -490,23 +490,21 @@ class LengowImport
                     $results = $this->connector->get(
                         '/v3.0/orders',
                         array(
-                            'marketplace_order_id'          => $this->marketplace_sku,
-                            'marketplace'                   => $this->marketplace_name,
-                            'account_id'                    => $this->account_id,
-                            'page'                          => $page,
-                            'marketplace_order_date_from'   => '2015-01-01T00:00:00+01:00'
-                            ),
+                            'marketplace_order_id' => $this->marketplace_sku,
+                            'marketplace'          => $this->marketplace_name,
+                            'account_id'           => $this->account_id,
+                            'page'                 => $page
+                        ),
                         'stream'
                     );
                 } else {
                     $results = $this->connector->get(
                         '/v3.0/orders',
                         array(
-                            'updated_from'                  => $this->date_from,
-                            'updated_to'                    => $this->date_to,
-                            'account_id'                    => $this->account_id,
-                            'page'                          => $page,
-                            'marketplace_order_date_from'   => $this->date_from
+                            'updated_from' => $this->date_from,
+                            'updated_to'   => $this->date_to,
+                            'account_id'   => $this->account_id,
+                            'page'         => $page
                         ),
                         'stream'
                     );
