65eb70f616a8b1388089ab7cfd7ac432219bcd40
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2018-02-26 16:43:46 +0100

    Hotfix: Deleting the shipping_date parameter in the action check request

--- classes/models/LengowMarketplace.php
+++ classes/models/LengowMarketplace.php
@@ -416,11 +416,12 @@ class LengowMarketplace
             $params['action_type'] = $action;
             $sendAction = true;
             // check if action is already created
-            $result = LengowConnector::queryApi(
-                'get',
-                '/v3.0/orders/actions/',
-                array_merge($params, array("queued" => "True"))
-            );
+            $getParams = array_merge($params, array('queued' => 'True'));
+            // array key deletion for verification in get
+            if (isset($getParams['shipping_date'])) {
+                unset($getParams['shipping_date']);
+            }
+            $result = LengowConnector::queryApi('get', '/v3.0/orders/actions/', $getParams);
             if (isset($result->error) && isset($result->error->message)) {
                 throw new LengowException($result->error->message);
             }
