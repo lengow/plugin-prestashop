88beb199fca00e97033e3d3bf6ad8fc3f961e0d0
alexis.hermanns <alexis.hermanns@pcp-065>
2024-02-02 09:43:08 +0100

    PST-20525: fix product_price, original_price in orderDetail

--- lengow.php
+++ lengow.php
@@ -42,7 +42,7 @@ class Lengow extends Module
     {
         $this->name = 'lengow';
         $this->tab = 'export';
-        $this->version = '3.4.6';
+        $this->version = '3.4.7';
         $this->author = 'Lengow';
         $this->module_key = '92f99f52f2bc04ed999f02e7038f031c';
         $this->ps_versions_compliancy = [
