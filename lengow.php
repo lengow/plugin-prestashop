a0752802ed6b583f463d8c452aae50a16d5d200f
alexis.hermanns <alexis.hermanns@pcp-065>
2024-05-24 14:32:26 +0200

    make release 3.5.2 in progress

--- lengow.php
+++ lengow.php
@@ -42,7 +42,7 @@ class Lengow extends Module
     {
         $this->name = 'lengow';
         $this->tab = 'export';
-        $this->version = '3.5.1';
+        $this->version = '3.5.2';
         $this->author = 'Lengow';
         $this->module_key = '92f99f52f2bc04ed999f02e7038f031c';
         $this->ps_versions_compliancy = [
