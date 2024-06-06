b9da7b0763a160baeeb9c449f3458f628f6686a7
alexis.hermanns <alexis.hermanns@pcp-065>
2024-06-06 17:16:41 +0200

    ECP-81:[WIP] unit tests in prestashop module

---
+++ tests/Unit/classes/models/LengowOrderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Lengow\Connector\Test\Unit\Model;
+
+use PHPUnit\Framework\TestCase;
+use LengowOrder;
+
+class LengowOrderTest extends TestCase
+{
+    /**
+     *
+     * @var LengowOrder
+     */
+    protected $order;
+
+    /**
+     * setup
+     *
+     * @return void
+     */
+    public function setup(): void
+    {
+       $this->order = new LengowOrder();
+    }
+
+    /**
+     * test class
+     */
+    public function testClassInstantiation()
+    {
+        $this->assertInstanceOf(
+            LengowOrder::class,
+            $this->order,
+            '[Test Class Instantiation] Check class instantiation'
+        );
+    }
+}
