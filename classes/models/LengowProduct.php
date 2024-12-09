ba8b29ab38aa1d37b8100e9a73517f3da9257fb1
alherlengow <151618393+alherlengow@users.noreply.github.com>
2024-12-09 16:53:29 +0100

    fix(export): [PST-22027] ignore Root category from breadcrumb (#33)

--- classes/models/LengowProduct.php
+++ classes/models/LengowProduct.php
@@ -481,6 +481,9 @@ class LengowProduct extends Product
             $breadcrumb = '';
             $categories = $this->categoryDefault->getParentsCategories();
             foreach ($categories as $category) {
+                if (isset($category['id_parent']) && $category['id_parent'] === 0) {
+                    continue;
+                }
                 $breadcrumb = $category['name'] . ' > ' . $breadcrumb;
             }
             $breadcrumb = rtrim($breadcrumb, ' > ');
